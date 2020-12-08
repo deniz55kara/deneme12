eeea5728bbe50eab5678798f3a8eb69d08fd2c6f
deniz55kara <denizbaro61@gmail.com>
2020-12-08 16:33:59 +0300

    First Windows Form

---
+++ FirstForm/FirstForm/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace FirstForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PROCESSOR_COMBOBOX = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // PROCESSOR_COMBOBOX
+            // 
+            this.PROCESSOR_COMBOBOX.FormattingEnabled = true;
+            this.PROCESSOR_COMBOBOX.Location = new System.Drawing.Point(260, 42);
+            this.PROCESSOR_COMBOBOX.Name = "PROCESSOR_COMBOBOX";
+            this.PROCESSOR_COMBOBOX.Size = new System.Drawing.Size(121, 21);
+            this.PROCESSOR_COMBOBOX.TabIndex = 2;
+            this.PROCESSOR_COMBOBOX.TabStop = false;
+            this.PROCESSOR_COMBOBOX.Text = "PROCESSOR";
+            this.PROCESSOR_COMBOBOX.SelectedIndexChanged += new System.EventHandler(this.PROCESSOR_COMBOBOX_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(57, 43);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(126, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(57, 122);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SHOW";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 451);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PROCESSOR_COMBOBOX);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "FIRST PROJECT";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox PROCESSOR_COMBOBOX;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ FirstForm/FirstForm/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FirstForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PROCESSOR_COMBOBOX.Items.Add("Intel i7");
+            PROCESSOR_COMBOBOX.Items.Add("Intel i3");
+            PROCESSOR_COMBOBOX.Items.Add("Intel i5");
+            PROCESSOR_COMBOBOX.Items.Add("Ryzen 3 3100");
+
+
+
+
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            MessageBox.Show("bbbbb");
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("tıkklandı");
+        }
+
+        private void PROCESSOR_COMBOBOX_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            string islemci;
+            islemci = (string)PROCESSOR_COMBOBOX.SelectedItem;
+
+            
+
+            if (string.Equals(islemci, "Intel i3"))  
+            {
+                textBox1.Text = "GİRİS SEVİYE";
+
+            }
+
+            if (string.Equals(islemci, "Intel i7"))
+            {
+                textBox1.Text = "OYUN BİLGİSAYARI";
+
+            }
+
+            if (string.Equals(islemci, "Ryzen 3 3100"))
+            {
+                textBox1.Text = "GİRİŞ SEVİYE";
+
+            }
+
+            if (string.Equals(islemci, "Intel i5"))
+            {
+                textBox1.Text = "İŞ BİLGİSAYARI";
+
+            }
+
+
+
+
+
+
+
+        }
+
+        
+    }
+}
